1963e21bf5ea6dd27fd795fed0f552f0fba4bf49
Virtual <virtualll001@gmail.com>
2022-01-12 12:08:27 +0100

    Add project files.

---
+++ KONZOLE_naPohovor/Pole.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KONZOLE_naPohovor
+{
+    //Pohovorová otázka: Je možné do pole ukládat různé typy?
+    //Odpověď ANO. Předkem všech typů je objekt - vytvoříme-li pole objektů, můžeme tam dát cokoliv:
+    public class Pole
+    {
+        public string MojePole()
+        {
+            //pole objektů
+            object[] poleCehokoliv = new object[3];
+            poleCehokoliv[0] = 21;
+            poleCehokoliv[1] = "text";
+
+            Osoba Kaja = new Osoba();
+            Kaja.Jmeno = "Karel Novák";
+            Kaja.Vek = 43;
+
+            poleCehokoliv[2] = Kaja;
+            string vystup = "";
+            foreach(object obj in poleCehokoliv)            
+                vystup += obj + ", " ;
+            
+            return vystup;
+        }
+
+        public string PoleList()
+        {
+            //můžeme také použít ARRAY LIST pracuje s objekty - vyžaduje using System.Collection
+            ArrayList poleListCehokoliv = new ArrayList(); 
+            poleListCehokoliv.Add(202);
+            poleListCehokoliv.Add("textové sdělení");           
+            poleListCehokoliv.Add(new Osoba { Jmeno = "Ema Young", Vek = 25 });
+            string vystup2 = "";
+            foreach (object obj in poleListCehokoliv)
+                vystup2 += obj + ", ";
+
+            return vystup2;
+        }
+
+        public override string ToString()
+        {
+            return PoleList().ToString(); //MojePole().ToString();
+        }
+    }
+
+    class Osoba
+    {
+        public string Jmeno { get; set; }
+        public int Vek { get; set; }
+
+        public override string ToString()
+        {
+            return $"Zdravím, jsem osoba: {Jmeno} a je mi {Vek} let!";
+        }
+    }
+}
---
+++ KONZOLE_naPohovor/Program.cs
@@ -0,0 +1,207 @@
+﻿using KONZOLE_naPohovor;
+
+//ZDROJ: https://www.youtube.com/watch?v=-1NUQNSVVmk&list=PL6n9fhu94yhWlAv3hnHzOaMSeggILsZFs&index=2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////6.
+////Pohovorová otázka: "Je možné uložit různé množství Listů různého typu v jednom generickém listu?"
+////Odpověď: Ano - List může obsahovat <List<object>>:
+
+//List<List<object>> list = new List<List<object>>();
+//List<object> list2 = new List<object>();
+//list2.Add("Mája"); list2.Add("Kája"); list2.Add("Bája"); list2.Add("Ája");
+//list.Add(list2);
+
+//List<object> list3 = new List<object>();
+//list3.Add(101); list3.Add(150); list3.Add(400);
+//list.Add(list3);
+
+//foreach (var item in list)
+//{
+//    foreach (var polozka in item)
+//    {
+//        Console.WriteLine(polozka);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+
+
+////5.
+////Pohovorová otázka: "Co je rekurzivní funkce (+ příklad)?"
+////Odpověď: Když metoda volá sama sebe:
+//
+////*******FAKTORIÁL ZA POMOCI REKURZE***********
+//Console.WriteLine("Prosím zadejte číslo: ");
+//int number = int.Parse(Console.ReadLine());
+//double fact = FaktorialRekurze.FactorialRekurze(number);
+//Console.WriteLine($"Faktoriál čísla {number} je: " + fact);
+//class FaktorialRekurze
+//{
+//    public static double FactorialRekurze(int number)
+//    {
+//        if (number == 0)
+//            return 1;
+//        return number * FactorialRekurze(number - 1);
+//    }
+//}
+//
+////*******FAKTORIÁL BEZ REKURZE***********
+//class Faktorial
+//{
+//    public static double Factorial(int number)
+//    {
+//        if (number == 0)
+//            return 1;
+//        double factorial = 1;
+//        for (int i = number; i >= 1; i--)
+//        {
+//            factorial = factorial * i;
+//        }
+//        return factorial;
+//    }
+//}
+//
+////*******FAKTORIÁL příklad 2 REKURZE pro hledání souborů***********
+//static void NajdiSoubory(string path)
+//{
+//    foreach (string fileName in Directory.GetFiles(path))
+//    {
+//        Console.WriteLine(fileName);
+//    }
+//    foreach(string directory in Directory.GetDirectories(path))
+//    {
+//        NajdiSoubory(directory);
+//    }
+//}
+//
+//Console.WriteLine("Please enter path: ");
+//string cesta = Console.ReadLine();
+//NajdiSoubory(cesta);
+////C:\Users\lonet\source\repos\KONZOLE
+
+
+
+
+
+//4.
+//Pohovorová otázka: "Kdy a proč použít interfacy?"
+//Odpověď:
+//1) díky nim vytváříme "loosly coupled" systémy
+//2) jsou užitečné pro DI
+//3) snadnější testování (unit testing a mocking)
+
+
+
+
+
+
+//3.
+//Pohovorová otázka: "Kdy a proč použít abstraktní třídu?"
+//Odpověď: Když máme dvě a více tříd se stejnou funkcionalitou (vlastnostmi a metodami)
+//a chceme se vyhnout duplicitnímu kódu, ohlídat si že odvozené třídy budou mít metody, které mít mají (abstract/override)
+//a zároveň daná třída nebude mít vlastní instance. 
+//Pokud chceme, aby základní třída měla své instance => řešímě běžným děděním (u metod virtual/override)...
+
+
+
+
+
+
+////2.
+////Pohovorová otázka: Co je "jagged array". Vypracuj příklad:
+////Odpověď: "zubaté pole" (pole polí, 2D pole) pole uvnitř pole:
+
+//string[] uchazeci = new string[] { "Machovská", "Novotný", "Douda" };
+//string[][] polePoli = new string[3][];
+
+//polePoli[0] = new string[3] { "střední škola", "bakalář", "doktor" };
+//polePoli[1] = new string[1] { "střední škola" };
+////delší zápis:
+//polePoli[2] = new string[2];
+//polePoli[2][0] = "střední škola";
+//polePoli[2][1] = "bakalář";
+
+//for (int i = 0; i < polePoli.Length; i++)
+//{
+//    Console.WriteLine(uchazeci[i]);
+//    Console.WriteLine("*********");
+//    string[] vnitrniPole = polePoli[i]; //je potřeba vytvořit si 
+//    for (int j = 0; j < vnitrniPole.Length; j++)
+//    {
+//        Console.WriteLine(vnitrniPole[j]);
+//    }
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("*************************************************");
+//Console.WriteLine();
+
+////KRATŠÍ ZÁPIS
+
+//string[] ovoce = { "Jahoda", "Banán", "Hruška" };
+//string[][] polePoliZKRATKA = new string[][]{
+//    new string[] { "vit. C", "Vit. A", "Vit. E" },
+//    new string[] { "draslík", "vápník"},
+//    new string[] { "vápník"}
+//};
+
+//for (int i = 0; i < polePoliZKRATKA.Length; i++)
+//{
+//    Console.WriteLine(ovoce[i]);
+//    Console.WriteLine("--------");
+//    string[] vnitrniPole2 = polePoliZKRATKA[i];
+//    for (int j = 0; j < vnitrniPole2.Length; j++)
+//    {
+//        Console.WriteLine(vnitrniPole2[j]);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+
+//1.
+//Pohovorová otázka: Je možné do pole ukládat různé typy?
+//Odpověď ANO. Předkem všech typů je objekt - vytvoříme-li pole objektů, můžeme tam dát cokoliv:
+//Viz třída Pole.cs
+//Pole pole = new Pole();
+//Console.WriteLine(pole);
+
